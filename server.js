70160ea277a205e9a24362539d1021093bc7944f
Vincent Tran <vintran1995@gmail.com>
2021-04-11 02:11:01 -0700

    finished server endpoints

--- server.js
+++ server.js
@@ -19,17 +19,37 @@ app.use(express.static('frontend'));
 app.use(express.json());
 //
 
+// GET - Retrieve all Workouts
 app.get('/api/v1/workouts', (req, res) => {
     // const createTableQuery = "CREATE TABLE IF NOT EXISTS Workout (id INT AUTO_INCREMENT, name VARCHAR(255), category VARCHAR(255), instructions VARCHAR(1024), equipment VARCHAR(255), amounts INT, PRIMARY KEY (id))";
     // const drop = 'DROP TABLE Workout';
 
-    const sql = 'SELECT * FROM Workout';
+    // const createTableQuery = "CREATE TABLE IF NOT EXISTS Session (id INT AUTO_INCREMENT, name VARCHAR(255), time FLOAT, PRIMARY KEY (id))";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('POST', '/api/v1/add_session', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('DELETE', '/api/v1/delete_session/:name', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('PUT', '/api/v1/update_session', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('POST', '/api/v1/search_id/:id', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('GET', '/api/v1/search_name/:name', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('POST', '/api/v1/search_fletter/:fletter', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('GET', '/api/v1/random', 0)";
+    // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('GET', '/api/v1/filter/:category', 0)";
 
-    con.query(sql, (err, result, fields) => {
+    // con.query(createStat, (err, result, fields) => {
+    //     if (err) {
+    //         console.log(err);
+    //         return res.sendStatus(503);
+    //     }
+    //     // console.log('Session table created');
+    //     console.log('Session search random');
+    //     return res.send();
+    // });
+
+    const getSQL = 'SELECT * FROM Workout';
+
+    con.query(getSQL, (err, result, fields) => {
         if (err) {
             console.log(err);
             return res.sendStatus(503);
-            // return reject(err);
         }
         if (result.length == 0) {
             console.log(JSON.stringify(result));
@@ -43,18 +63,20 @@ app.get('/api/v1/workouts', (req, res) => {
         con.query(updateStat, (statErr, statResult, statFields) => {
             if (statErr) {
                 console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
             }
             console.log(JSON.stringify(statResult));
         });
     
         console.log(JSON.stringify(result));
+        res.status(200);
         return res.send(JSON.stringify(result));
     });
-})
+});
 
+// POST - Create a new workout
 app.post('/api/v1/add_exercise', (req, res) => {
-    console.log("hi");
-    console.log(req.body);
     let name = req.body.name;
     let category = req.body.cat;
     let instructions = req.body.instructions;
@@ -66,9 +88,9 @@ app.post('/api/v1/add_exercise', (req, res) => {
         return res.send({'error': 'Missing parameters'});
     }
 
-    let sql = `INSERT INTO Workout (name, category, instructions, equipment, amounts) VALUES ('${name}', '${category}', '${instructions}', '${equipment}', '${amounts}')`;
+    let postSQL = `INSERT INTO Workout (name, category, instructions, equipment, amounts) VALUES ('${name}', '${category}', '${instructions}', '${equipment}', '${amounts}')`;
 
-    con.query(sql, (err, result, fields) => {
+    con.query(postSQL, (err, result, fields) => {
         if (err) {
             console.log(err);
             res.status(500);
@@ -80,17 +102,346 @@ app.post('/api/v1/add_exercise', (req, res) => {
         con.query(updateStat, (statErr, statResult, statFields) => {
             if (statErr) {
                 console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
             }
             console.log(JSON.stringify(statResult));
         });
 
         console.log('Successfully inserted to database');
-        res.status(200);
+        res.status(201);
         return res.send({msg: 'POST request successful'});
+    });
+});
+
+// GET - Get random workout
+app.get('/api/v1/random', (req, res) => {
+
+    const sql = `SELECT * FROM Workout ORDER BY RAND() LIMIT 1`;
+    const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/random'";
+
+    con.query(sql, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+            return res.send('No workouts found');
+        }
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully retrieved random workout');
+        res.status(200);
+        return res.send(JSON.stringify(result));
+    });
+});
+
+// POST - Get specific workout by first letter
+app.post('/api/v1/search_fletter/:fletter', (req, res) => {
+    const fletter = req.params.fletter;
+
+    if (!fletter) {
+        res.status(400);
+        return res.send({'error': 'Missing parameters'});
+    }
+
+    if (fletter.length > 1) {
+        res.status(400);
+        return res.send({'error': 'Search by first letter only accepts one letter'});
+    }
+
+    let sql = `SELECT * FROM Workout WHERE name LIKE '${fletter}%'`;
+
+    con.query(sql, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(404);
+            return res.send(`Workouts with first letter '${fletter}' could not be found`);
+        }
+
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/search_fletter/:fletter'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully retrieved workout from first letter');
+        res.status(200);
+        return res.send(JSON.stringify(result));
+    });
+});
+
+// GET - Get specific workout by name
+app.get('/api/v1/search_name/:name', (req, res) => {
+    const name = req.params.name;
+    console.log(name);
+
+    if (!name) {
+        res.status(400);
+        return res.send({'error': 'Missing parameters'});
+    }
+
+    let sql = `SELECT * FROM Workout WHERE name = '${name}'`;
+
+    con.query(sql, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(404);
+            return res.send('Workout name could not be found');
+        }
+
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/search_name/:name'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully retrieved workout from name');
+        console.log(name);
+        res.status(200);
+        return res.send(JSON.stringify(result));
+    });
+});
+
+// POST - Get specific workout by id
+app.post('/api/v1/search_id/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!id) {
+        res.status(400);
+        return res.send({'error': 'Missing parameters'});
+    }
+
+    const sql = `SELECT * FROM Workout WHERE id = ${id}`;
+    const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/search_id/:id'";
+
+    con.query(sql, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(404);
+            return res.send('Workout id could not be found');
+        }
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully retrieved workout from id');
+        res.status(200);
+        return res.send(JSON.stringify(result));
+    });
+});
+
+// GET - Get specific workout by category
+app.get('/api/v1/category/:category', (req, res) => {
+    const cat = req.params.category;
+
+    if (!cat) {
+        res.status(400);
+        return res.send({'error': 'Missing filter parameters'});
+    }
+
+    let sql = `SELECT * FROM Workout WHERE UPPER(category) = UPPER('${cat}')`;
+
+    con.query(sql, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(404);
+            return res.send(`Workouts with category '${cat}' could not be found`);
+        }
+
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/filter/:category'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully retrieved filtered workouts');
+        res.status(200);
+        return res.send(JSON.stringify(result));
+    });
+});
+
+// DELETE - Delete a workout
+app.delete('/api/v1/delete/:name', (req, res) => {
+    const name = req.params.name;
+
+    const deleteSQL = `DELETE FROM Workout WHERE name = '${name}'`;
+    con.query(deleteSQL, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(400);
+            return res.send('Workout name could not be found');
+        }
+
+        // const deleteStat = `DELETE FROM ApiStats WHERE endpoint = '/api/v1/delete/:name'`;
+        // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('DELETE', '/api/v1/delete/:name', 0)";
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/delete/:name'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully deleted item from database');
+        res.status(200);
+        return res.send({msg: 'DELETE request successful'});
     })
+});
 
-})
+// PUT - Update a workout's name
+app.put('/api/v1/update', (req, res) => {
+    const oldName = req.body.oldName;
+    const newName = req.body.newName;
 
+    const updateSQL = `UPDATE Workout SET Workout.name = '${newName}' WHERE Workout.name = '${oldName}'`;
+    con.query(updateSQL, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(400);
+            return res.send('Workout name could not be found');
+        }
+
+        // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('PUT', '/api/v1/update', 0)";
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/update'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully updated database');
+        res.status(200);
+        return res.send({msg: 'PUT request successful'});
+    })
+});
+
+// POST - Create a new workout session
+app.post('/api/v1/add_session', (req, res) => {
+    const name = req.body.name;
+    const time = req.body.time;
+
+    if (!name || !time) {
+        res.status(400);
+        return res.send({'error': 'Missing parameters'});
+    }
+
+    // const createTableQuery = "CREATE TABLE IF NOT EXISTS Session (id INT AUTO_INCREMENT, name VARCHAR(255), time FLOAT, PRIMARY KEY (id))";
+
+    let postSQL = `INSERT INTO Session (name, time) VALUES ('${name}', '${time}')`;
+
+    con.query(postSQL, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(500);
+            return res.send('Something went wrong with the server');
+        }
+
+        // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('POST', '/api/v1/add_session', 0)";
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/add_session'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully inserted to database');
+        res.status(201);
+        return res.send({msg: 'POST request successful'});
+    });
+});
+
+// DELETE - Delete a workout session
+app.delete('/api/v1/delete_session/:name', (req, res) => {
+    const name = req.params.name;
+    console.log(name);
+
+    const deleteSQL = `DELETE FROM Session WHERE name = '${name}'`;
+    con.query(deleteSQL, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(400);
+            return res.send('Workout session name could not be found');
+        }
+
+        // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('DELETE', '/api/v1/delete_session/:name', 0)";
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/delete_session/:name'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully deleted item from database');
+        res.status(200);
+        return res.send({msg: 'DELETE request successful'});
+    })
+});
+
+// PUT - Update a workout session's time
+app.put('/api/v1/update_session', (req, res) => {
+    const name = req.body.name;
+    const time = req.body.time;
+
+    const updateSQL = `UPDATE Workout SET Workout.time = '${time}' WHERE Workout.name = '${name}'`;
+    con.query(updateSQL, (err, result, fields) => {
+        if (err) {
+            console.log(err);
+            res.status(400);
+            return res.send('Workout name could not be found');
+        }
+
+        // const createStat = "INSERT INTO ApiStats (method, endpoint, requests) VALUES ('PUT', '/api/v1/update_session', 0)";
+        const updateStat = "UPDATE ApiStats SET requests = requests + 1 WHERE endpoint = '/api/v1/update_session'";
+        con.query(updateStat, (statErr, statResult, statFields) => {
+            if (statErr) {
+                console.log(statErr); 
+                res.status(500);
+                return res.send('Something went wrong with the server');
+            }
+            console.log(JSON.stringify(statResult));
+        });
+
+        console.log('Successfully updated database');
+        res.status(200);
+        return res.send({msg: 'PUT request successful'});
+    })
+});
+
+// GET - Retrieve all API endpoint stats
 app.get('/api/v1/admin', (req, res) => {
     // const createTableQuery = "CREATE TABLE IF NOT EXISTS ApiStats (id INT AUTO_INCREMENT, method VARCHAR(255), endpoint VARCHAR(255), requests INT, PRIMARY KEY (id))";
 
